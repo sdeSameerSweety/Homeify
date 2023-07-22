b8da13987e77bbb9b15e21f4b42819d50b886ab7
Aditya Kumar <ceo@sameerswain.com>
2023-07-22 18:47:35 +0530

    homepage_image added

---
+++ homeify/src/components/Main/Homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.css";
+
+const Homepage = () => {
+  return (
+    <div className="banner">
+      <img src="/assets/images/top_img.jpg" />
+    </div>
+  );
+};
+
+export default Homepage;
