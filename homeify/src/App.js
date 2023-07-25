2daeb380b4a9c77103f17606872c5e38645f3bac
Aditya Kumar <ceo@sameerswain.com>
2023-07-25 23:06:08 +0530

    dummy and schema changes

--- homeify/src/App.js
+++ homeify/src/App.js
@@ -6,22 +6,23 @@ import TrackPage from "./components/Main/ProfileActions/TrackPage";
 import WalletPage from "./components/Main/ProfileActions/WalletPage";
 import Navbar from "./components/Main/Navbar/Navbar";
 import { ErrorPage } from "./components/Main/ErrorPage/ErrorPage";
+import Dummy from "./components/Personal/Aditya/Dummy/Dummy";
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:8080';
-axios.defaults.withCredentials=true;
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/profile" element={<ProfilePage/>}/>
-      <Route path="/orders" element={<TrackPage/>}/>
-      <Route path="/wallet" element={<WalletPage/>}/>
-      <Route path="*" element={<ErrorPage/>}/>
-    </Routes>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/orders" element={<TrackPage />} />
+        <Route path="/wallet" element={<WalletPage />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/dummy" element={<Dummy />} />
+      </Routes>
     </>
   );
 }
