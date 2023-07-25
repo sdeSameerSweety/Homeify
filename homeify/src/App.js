8f9a9e7bd27a98679cf2a0b951e6be6ed38735f9
Aditya Kumar <ceo@sameerswain.com>
2023-07-25 22:58:01 +0530

    commited corrections

--- homeify/src/App.js
+++ homeify/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
     <UserContextProvider>
-    <Navbar/>
+    
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/profile" element={<ProfilePage/>}/>
