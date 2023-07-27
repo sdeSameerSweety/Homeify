30c26617b149fd4d07eb3a554a12e1859be37f19
Aditya Kumar <ceo@sameerswain.com>
2023-07-27 15:35:47 +0530

    All fine after merge now

--- homeify/src/components/Main/ProfileActions/AddressAdditon.js
+++ homeify/src/components/Main/ProfileActions/AddressAdditon.js
@@ -47,7 +47,7 @@ const AddressAdditon = () => {
     
    
     if (submit || redirect) {
-      return <Navigate to={"/"} />;
+      return <Navigate to={"/profile"} />;
     }
     return (
     <>
