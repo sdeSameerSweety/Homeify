aaa76a169561c0cdf564f19897522b16b2dcacd7
Aditya Kumar <ceo@sameerswain.com>
2023-07-27 19:33:28 +0530

    product page  css responsive

--- homeify/src/components/Personal/Somya/Cards/SofaProducts/Sort .jsx
+++ homeify/src/components/Personal/Somya/Cards/SofaProducts/Sort .jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 function Sort({ sortOrder, setSortOrder, handleOpen }) {
   function handleChange(e) {
     setSortOrder(e.target.value);
@@ -6,30 +7,34 @@ function Sort({ sortOrder, setSortOrder, handleOpen }) {
 
   return (
     <div className="sort_div">
-      <div id="filter_div">Filter By</div>
-      <div id="swatch_div">
-        <button className="button-24" role="button" onClick={handleOpen}>
-          Color Swatch
-        </button>
+      <div className="div1filter">
+        <div id="filter_div">Filter By</div>
+        <div id="swatch_div">
+          <button className="button-24" role="button" onClick={handleOpen}>
+            Color
+          </button>
+        </div>
+        <div id="brand_div">
+          <button className="button-24" role="button">
+            Brand
+          </button>
+        </div>
+        <div id="margin_div">
+          <button className="button-24" role="button">
+            Margin
+          </button>
+        </div>
       </div>
-      <div id="brand_div">
-        <button className="button-24" role="button">
-          Brand
-        </button>
-      </div>
-      <div id="margin_div">
-        <button className="button-24" role="button">
-          Margin
-        </button>
-      </div>
-      <div id="sort_head">
-        <h3>Sort By</h3>
-      </div>
-      <div id="sort_option">
-        <select onChange={handleChange}>
-          <option value={1}>Lowest to Highest</option>
-          <option value={2}>Highest to Lowest</option>
-        </select>
+      <div className="div1sort">
+        <div id="sort_head">
+          <h3>Sort By</h3>
+        </div>
+        <div id="sort_option">
+          <select onChange={handleChange}>
+            <option value={1}>Lowest to Highest</option>
+            <option value={2}>Highest to Lowest</option>
+          </select>
+        </div>
       </div>
     </div>
   );
