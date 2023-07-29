6a8486647d54873939cff8740e290e324ea1e1a0
Aditya Kumar <ceo@sameerswain.com>
2023-07-29 12:04:17 +0530

    footer

--- homeify/src/index.js
+++ homeify/src/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import Footer1 from "./components/Main/Products/Addons/footer/footer1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-
+  <BrowserRouter>
+    <App />
+    <Footer1 />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
